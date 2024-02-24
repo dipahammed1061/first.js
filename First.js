71f061e1311db14c72c36d30a5f8a8cf88da552f
dipahammed1061 <mddip008684@gmail.com>
2024-02-24 23:18:26 +0600

    fist

---
+++ First.js
@@ -0,0 +1,19 @@
+let age= 24;
+let price =100.25;
+let fullName = "Dip Ahammed";
+isFollow = true;
+let x;
+let y = null;
+let z = BigInt("12345");
+let p = Symbol ("Hello !")
+
+const student ={
+    fullName : "Dip Ahammed",
+    age : 20,
+    cgpa : 3.2,
+    isPass : true
+};
+
+student.age = student.age+2;
+student.fullName ="Dip";
+console.log(student.age,student.fullName);
